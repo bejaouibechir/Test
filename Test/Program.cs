1d3ae1404e9bb1c818ab44b88f63f3f3f362d54f
bechir <bechir@PC2022>
2022-01-12 22:34:04 +0100

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test
+{
+    public class Program
+    {
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("Ce ci est une application de test");
+
+            Console.Read();
+            
+        }
+    }
+}
